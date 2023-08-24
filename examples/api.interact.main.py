56a6815e25d585f5534f8f08aad1804d8b922afa
fivetran-elijahdavis <104081720+fivetran-elijahdavis@users.noreply.github.com>
2023-08-24 10:31:06 -0600

    Update api.interact.main.py

--- examples/api.interact.main.py
+++ examples/api.interact.main.py
@@ -9,14 +9,24 @@ api_secret = ''
 a = HTTPBasicAuth(api_key, api_secret)
 
 def atlas(method, endpoint, payload):
-
+    
+    # Base URL for the Fivetran API
     base_url = 'https://api.fivetran.com/v1'
+    
+    # Set up headers with authorization
     h = {
         'Authorization': f'Bearer {api_key}:{api_secret}'
     }
+    # Construct full URL
     url = f'{base_url}/{endpoint}'
 
     try:
+        # Make the request using the specified method
+        # If method is not one of the expected values, raise a ValueError
+        # If the request fails for any reason, catch the exception and print an error message
+        # If the HTTP status code indicates an error, raise an exception
+        # If the request is successful, return the JSON response
+        
         if method == 'GET':
             response = requests.get(url, headers=h, auth=a)
         elif method == 'POST':
@@ -37,6 +47,8 @@ def atlas(method, endpoint, payload):
 
 
 if __name__ == '__main__':
+    
+    #define parameters
     connector_id = ''
     method = 'PATCH'  #'POST' 'PATCH' 'DELETE' 'GET'
     endpoint = 'connectors/' + connector_id 
