328f47ec4d6061b50038c4bb6696cece2852b2d6
fivetran-elijahdavis <104081720+fivetran-elijahdavis@users.noreply.github.com>
2023-08-29 12:09:25 -0600

    Create api_interact_status_cursor.py

---
+++ examples/api_interact_status_cursor.py
@@ -0,0 +1,66 @@
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+import colorama
+from colorama import Fore, Back, Style
+
+api_key = ''
+api_secret = ''
+a = HTTPBasicAuth(api_key, api_secret)
+
+def atlas(method, endpoint, payload, cursor=''):
+
+    base_url = 'https://api.fivetran.com/v1'
+    h = {
+        'Authorization': f'Bearer {api_key}:{api_secret}'
+    }
+    url = f'{base_url}/{endpoint}?cursor={cursor}'
+
+    try:
+        if method == 'GET':
+            response = requests.get(url, headers=h, auth=a)
+        elif method == 'POST':
+            response = requests.post(url, headers=h, json=payload, auth=a)
+        elif method == 'PATCH':
+            response = requests.patch(url, headers=h, json=payload, auth=a)
+        elif method == 'DELETE':
+            response = requests.delete(url, headers=h, auth=a)
+        else:
+            raise ValueError('Invalid request method.')
+
+        response.raise_for_status()  # Raise exception for 4xx or 5xx responses
+
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        print(f'Request failed: {e}')
+        return None
+
+#Request:
+group_id = ''
+method = 'GET'
+endpoint = 'groups/' + group_id + '/connectors'
+payload = ''
+
+#Submit
+response = atlas(method, endpoint, payload)
+#Review
+if response is not None:
+    print(Fore.CYAN + 'Call: ' + method + ' ' + endpoint + ' ' + str(payload))
+    print(Fore.GREEN + 'Response: ' + response['code'])
+    cdata_list =  response['data']
+    ctimeline  =  cdata_list['items']
+    for c in ctimeline:
+        print(Fore.MAGENTA + 'Type:' + c['service'] + Fore.BLUE + ' Status:' + c['status']['sync_state'] + Fore.YELLOW + ' Frequency:' + str(c['sync_frequency']))
+   
+  # Pagination
+    next_cursor = cdata_list.get('next_cursor', 'none')
+    print(next_cursor)  
+    while next_cursor != 'none':
+        response = atlas(method, endpoint, payload, next_cursor)
+        if response is not None:
+            cdata_list =  response['data']
+            ctimeline  =  cdata_list['items']
+            for c in ctimeline:
+                print(Fore.MAGENTA + 'Type:' + c['service'] + Fore.BLUE + ' Status:' + c['status']['sync_state'] + Fore.YELLOW + ' Frequency:' + str(c['sync_frequency']))
+            next_cursor = cdata_list.get('next_cursor', 'none')
+        print(next_cursor)  
