5ae72fbc6840617f4348368a99dce2be4424c320
fivetran-elijahdavis <104081720+fivetran-elijahdavis@users.noreply.github.com>
2023-08-29 10:18:34 -0600

    Create api_interact_new_team.py

---
+++ examples/api_interact_new_team.py
@@ -0,0 +1,55 @@
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+import colorama
+from colorama import Fore
+
+api_key = ''
+api_secret = ''
+a = HTTPBasicAuth(api_key, api_secret)
+
+#create new team for rbac
+
+def atlas(method, endpoint, payload):
+
+    base_url = 'https://api.fivetran.com/v1'
+    h = {
+        'Authorization': f'Bearer {api_key}:{api_secret}'
+    }
+    url = f'{base_url}/{endpoint}'
+
+    try:
+        if method == 'GET':
+            response = requests.get(url, headers=h, auth=a)
+        elif method == 'POST':
+            response = requests.post(url, headers=h, json=payload, auth=a)
+        elif method == 'PATCH':
+            response = requests.patch(url, headers=h, json=payload, auth=a)
+        elif method == 'DELETE':
+            response = requests.delete(url, headers=h, auth=a)
+        else:
+            raise ValueError('Invalid request method.')
+
+        response.raise_for_status()  # Raise exception
+
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        print(f'Request failed: {e}')
+        return None
+
+#Request
+method = 'POST'  #'POST' 'PATCH' 'DELETE' 'GET'
+endpoint = 'teams'
+payload = {
+    "name": "Test Finance Team",
+    "description": "Test Finance Team description",
+    "role": "Test Account Analyst"
+    }
+
+#Submit
+response = atlas(method, endpoint, payload)
+#Review
+if response is not None:
+    print(Fore.CYAN + 'Call: ' + method + ' ' + endpoint + ' ' + str(payload))
+    print(Fore.GREEN +  'Response: ' + response['code'])
+    print(Fore.MAGENTA + str(response))
