9b8303f18ec5360ed8da21a4f04827e6cf0db5c8
fivetran-elijahdavis <104081720+fivetran-elijahdavis@users.noreply.github.com>
2024-04-12 08:51:09 -0600

    Create api_interact_setup_test.py

---
+++ examples/api_interact_setup_test.py
@@ -0,0 +1,62 @@
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+import colorama
+from colorama import Fore, Back, Style
+
+#configuration file
+r = '/config.json'
+with open(r, "r") as i:
+    l = i.read()
+    y = json.loads(l)
+api_key = y['fivetran']['api_key']
+api_secret = y['fivetran']['api_secret']
+a = HTTPBasicAuth(api_key, api_secret)
+
+#api_key = ''
+#api_secret = ''
+#a = HTTPBasicAuth(api_key, api_secret)
+
+#Copy a Connector
+def atlas(method, endpoint, payload):
+
+    base_url = 'https://api.fivetran.com/v1'
+    h = {
+        'Authorization': f'Bearer {api_key}:{api_secret}'
+    }
+    url = f'{base_url}/{endpoint}'
+
+    try:
+        if method == 'GET':
+            response = requests.get(url, headers=h, auth=a)
+        elif method == 'POST':
+            response = requests.post(url, headers=h, json=payload, auth=a)
+        elif method == 'PATCH':
+            response = requests.patch(url, headers=h, json=payload, auth=a)
+        elif method == 'DELETE':
+            response = requests.delete(url, headers=h, auth=a)
+        else:
+            raise ValueError('Invalid request method.')
+
+        response.raise_for_status()  # Raise exception for 4xx or 5xx responses
+
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        print(f'Request failed: {e}')
+        return None
+
+#Request to run setup tests for a given connector
+connector_id = '' 
+method = 'POST'  #'POST' 'PATCH' 'DELETE'
+endpoint = 'connectors/'+ connector_id + '/test' 
+payload = {
+    "trust_certificates": True,
+    "trust_fingerprints": True
+}
+
+
+#Submit
+response = atlas(method, endpoint, payload)
+#Review
+if response is not None:
+    print(response)
